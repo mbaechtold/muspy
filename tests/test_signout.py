a810b576263847e17c1553b5dbac4ffe689cef4d
Martin Bächtold <martin@baechtold.me>
2018-11-06 18:30:34 +0100

    Test the sign out functionality.

---
+++ tests/test_signout.py
@@ -0,0 +1,15 @@
+from django_webtest import WebTest
+
+
+class TestSignOut(WebTest):
+    """
+    A user must be able to sign out.
+    """
+    def test_sign_out(self):
+        response = self.app.get("/", user='john.doe')
+        assert response.status == "200 OK"
+        assert response.context["user"].is_anonymous == False
+        assert response.context["user"].username == u'john.doe'
+
+        response = response.click('Sign out').follow()
+        assert response.context["user"].is_anonymous == True
