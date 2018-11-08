34e938c9ac8e590cb96f9e70e5ba97ed4a4aeca9
Martin Bächtold <martin@baechtold.me>
2018-11-08 08:20:27 +0100

    Switch to "django-configurations".
    The main advantage are deployment specific settings powered by class based settings and built-in support for "envdir" and thus environment variables.

--- settings.py
+++ settings.py
@@ -17,136 +17,140 @@
 
 import os
 
+from configurations import Configuration
 import dj_database_url
 
-########################################################################
-# Change the next section in production
-########################################################################
 
-DEBUG = True
+class Base(Configuration):
 
-SECRET_KEY = 'change me'
+    ########################################################################
+    # Change the next section in production
+    ########################################################################
 
-SERVER_EMAIL = 'info@muspy.com'
-EMAIL_HOST = 'localhost'
-EMAIL_PORT = 1025
+    DEBUG = True
 
-LASTFM_API_KEY='change me'
+    SECRET_KEY = 'change me'
 
-########################################################################
+    SERVER_EMAIL = 'info@muspy.com'
+    EMAIL_HOST = 'localhost'
+    EMAIL_PORT = 1025
 
-ADMINS = (('admin', 'info@muspy.com'),)
-MANAGERS = ADMINS
-SEND_BROKEN_LINK_EMAILS = True
+    LASTFM_API_KEY='change me'
 
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+    ########################################################################
 
-DATABASES = {
-    'default': dj_database_url.parse('sqlite:///db/muspy.db')
-}
+    ADMINS = (('admin', 'info@muspy.com'),)
+    MANAGERS = ADMINS
+    SEND_BROKEN_LINK_EMAILS = True
 
-TIME_ZONE = None
-USE_I18N = False
-LOGIN_REDIRECT_URL = '/artists'
-LOGIN_URL = '/signin'
-AUTH_PROFILE_MODULE = 'app.UserProfile'
-MESSAGE_STORAGE = 'django.contrib.messages.storage.cookie.CookieStorage'
-ROOT_URLCONF = 'urls'
-EMAIL_SUBJECT_PREFIX = '[muspy] '
-TEMPLATES = [
-    {
-        "APP_DIRS": True,
-        "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [os.path.join(BASE_DIR, "templates")],
-        "OPTIONS": {
-            "debug": DEBUG,
-            "context_processors": [
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
-                'django.template.context_processors.csrf',
-                'django.template.context_processors.debug',
-                'django.template.context_processors.request',
-            ]
-        }
+    BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
+    DATABASES = {
+        'default': dj_database_url.parse('sqlite:///db/muspy.db')
     }
-]
-AUTHENTICATION_BACKENDS = ('app.backends.EmailAuthBackend',)
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-MIDDLEWARE = (
-    'django.middleware.common.CommonMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-)
 
-INSTALLED_APPS = (
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
-    'django.contrib.sites',
-    'piston',
-    'app',
-)
-
-SITE_ID = 1
-
-STATIC_URL = '/static/'
-
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
-]
-
-PASSWORD_HASHERS = [
-    'django.contrib.auth.hashers.PBKDF2PasswordHasher',
-    'django.contrib.auth.hashers.PBKDF2SHA1PasswordHasher',
-    'django.contrib.auth.hashers.SHA1PasswordHasher',
-]
-
-LOGGING = {
-    'version': 1,
-    'disable_existing_loggers': False,
-    'formatters': {
-        'verbose': {
-            'format': '%(levelname)s %(asctime)s %(module)s %(process)d %(thread)d %(message)s'
-        },
-        'simple': {
-            'format': '%(levelname)s %(message)s'
-        },
-    },
-    'handlers': {
-        'console': {
-            'level': 'DEBUG',
-            'class': 'logging.StreamHandler',
-            'formatter': 'verbose'
-        },
-        'mail_admins': {
-            'level': 'ERROR',
-            'class': 'django.utils.log.AdminEmailHandler'
+    TIME_ZONE = None
+    USE_I18N = False
+    LOGIN_REDIRECT_URL = '/artists'
+    LOGIN_URL = '/signin'
+    AUTH_PROFILE_MODULE = 'app.UserProfile'
+    MESSAGE_STORAGE = 'django.contrib.messages.storage.cookie.CookieStorage'
+    ROOT_URLCONF = 'urls'
+    EMAIL_SUBJECT_PREFIX = '[muspy] '
+    TEMPLATES = [
+        {
+            "APP_DIRS": True,
+            "BACKEND": "django.template.backends.django.DjangoTemplates",
+            "DIRS": [os.path.join(BASE_DIR, "templates")],
+            "OPTIONS": {
+                "debug": DEBUG,
+                "context_processors": [
+                    'django.contrib.auth.context_processors.auth',
+                    'django.contrib.messages.context_processors.messages',
+                    'django.template.context_processors.csrf',
+                    'django.template.context_processors.debug',
+                    'django.template.context_processors.request',
+                ]
+            }
         }
-    },
-    'loggers': {
-        '': {
-            'handlers': ['console'],
-            'level': 'WARNING',
+    ]
+    AUTHENTICATION_BACKENDS = ('app.backends.EmailAuthBackend',)
+    EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+    MIDDLEWARE = (
+        'django.middleware.common.CommonMiddleware',
+        'django.contrib.sessions.middleware.SessionMiddleware',
+        'django.middleware.csrf.CsrfViewMiddleware',
+        'django.contrib.auth.middleware.AuthenticationMiddleware',
+        'django.contrib.messages.middleware.MessageMiddleware',
+    )
+
+    INSTALLED_APPS = (
+        'django.contrib.admin',
+        'django.contrib.auth',
+        'django.contrib.contenttypes',
+        'django.contrib.sessions',
+        'django.contrib.messages',
+        'django.contrib.staticfiles',
+        'django.contrib.sites',
+        'piston',
+        'app',
+    )
+
+    SITE_ID = 1
+
+    STATIC_URL = '/static/'
+
+    STATICFILES_DIRS = [
+        os.path.join(BASE_DIR, "static"),
+    ]
+
+    PASSWORD_HASHERS = [
+        'django.contrib.auth.hashers.PBKDF2PasswordHasher',
+        'django.contrib.auth.hashers.PBKDF2SHA1PasswordHasher',
+        'django.contrib.auth.hashers.SHA1PasswordHasher',
+    ]
+
+    LOGGING = {
+        'version': 1,
+        'disable_existing_loggers': False,
+        'formatters': {
+            'verbose': {
+                'format': '%(levelname)s %(asctime)s %(module)s %(process)d %(thread)d %(message)s'
+            },
+            'simple': {
+                'format': '%(levelname)s %(message)s'
+            },
         },
-        'django.db': {
-            'handlers': ['console'],
-            'level': 'ERROR',
-            'propagate': False,  # required to avoid double logging with root logger
+        'handlers': {
+            'console': {
+                'level': 'DEBUG',
+                'class': 'logging.StreamHandler',
+                'formatter': 'verbose'
+            },
+            'mail_admins': {
+                'level': 'ERROR',
+                'class': 'django.utils.log.AdminEmailHandler'
+            }
         },
-        'django.request': {
-            'handlers': ['console'],
-            'level': 'ERROR',
-            'propagate': False,  # required to avoid double logging with root logger
+        'loggers': {
+            '': {
+                'handlers': ['console'],
+                'level': 'WARNING',
+            },
+            'django.db': {
+                'handlers': ['console'],
+                'level': 'ERROR',
+                'propagate': False,  # required to avoid double logging with root logger
+            },
+            'django.request': {
+                'handlers': ['console'],
+                'level': 'ERROR',
+                'propagate': False,  # required to avoid double logging with root logger
+            },
+            'app': {
+                'handlers': ['console'],
+                'level': 'DEBUG',
+                'propagate': False,  # required to avoid double logging with root logger
+            },
         },
-        'app': {
-            'handlers': ['console'],
-            'level': 'DEBUG',
-            'propagate': False,  # required to avoid double logging with root logger
-        },
-    },
-}
+    }
