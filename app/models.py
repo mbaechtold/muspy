3b7f26a6bc1473a117360d3afe7abce345922587
Martin Bächtold <martin@baechtold.me>
2018-12-02 10:21:25 +0100

    Prevent validation error in Django Admin.
    The fallback urls are considered invalid in the Django Admin if they contain a space.

--- app/models.py
+++ app/models.py
@@ -253,7 +253,7 @@ class ReleaseGroup(models.Model):
 
     """
 
-    FALLBACK_COVER_ART_URL = "https://via.placeholder.com/250x250.png?text=NOT FOUND"
+    FALLBACK_COVER_ART_URL = "https://via.placeholder.com/250x250.png?text=NOT%20FOUND"
 
     class Meta:
         unique_together = ("artist", "mbid")
